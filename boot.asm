c87f735c19be30a9b77b1f8c143f636d454b0e19
Seth Stephanz <sethstephanz@gmail.com>
2024-06-14 19:44:15 -0400

    Fix comment syntax and fill out bootload routine

--- boot.asm
+++ boot.asm
@@ -1,10 +1,16 @@
-// specify assembly origins
-ORG 0x7c00          // where bootloader loads to. should start at 0 then JMP but will do this for now
-BITS 16             // tell assembler that we're using 16-bit architecture
+; specify assembly origins
+ORG 0x7c00                          ; where bootloader loads to. should start at 0 then JMP but will do this for now
+BITS 16                             ; tell assembler that we're using 16-bit architecture
 
-// interrupt table: https://www.ctyme.com/intr/int.htm
+; interrupt table: https://www.ctyme.com/intr/int.htm
 start:
-    // basic BIOS routine
-    mov ah, 0eh     // assembly refresher: ah = high part of eax
+    ; basic BIOS routine
+    mov ah, 0eh                     ; assembly refresher: ah is high portion of eax
     mov al, 'A'
-    int 0x10        // set up interrupt
+    mov bx, 0
+    int 0x10                        ; set up interrupt cf. table
+
+    jmp $
+
+times 510- ($ - $$) db 0            ; fill at least 510 bytes of data, pad with 0s
+dw 0xAA55                           ; reminder: Intel machines are little-endian!
